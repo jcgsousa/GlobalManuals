a4b600d2d2eb804d5d81b95fcd521f4c85a748a7
JulioS <juliosousa@sapo.pt>
2024-02-21 00:43:27 +0000

    update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function openImage(imageUrl) {
   var windowName = 'ManualImageWindow';
   var windowFeatures = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=800,height=600';
-  window.open(imageUrl, windowName, windowFeatures);
+  window.open('','_blank').location.href = "url"
 }
