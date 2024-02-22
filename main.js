b8247fe6c60a06d50480d03ffb90b9a41ad08f5c
JulioS <juliosousa@sapo.pt>
2024-02-22 11:48:10 +0000

    auto ajuste da janela do browser à imagem

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
+//  function openImage(imageUrl) {
+//   var windowName = 'ManualImageWindow';
+//   var windowFeatures = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=800,height=600';
+//   window.open(imageUrl, '_blank', 'toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=800,height=600');
+//  }
+
+// teste abrir a imagem e ajustar a janela à dimensão da foto
 function openImage(imageUrl) {
-  var windowName = 'ManualImageWindow';
-  var windowFeatures = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=800,height=600';
-  window.open(imageUrl, '_blank', 'toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=800,height=600');
+  var img = new Image();
+  
+  img.onload = function() {
+    var width = img.width;
+    var height = img.height;
+    var windowFeatures = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=' + width + ',height=' + height;
+    window.open(imageUrl, '_blank', windowFeatures);
+  };
+  img.src = imageUrl;
 }
+
+openImage('./images/image01.jpg');
