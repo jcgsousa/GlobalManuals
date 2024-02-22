5421b15228525bba667419bb4c5e65431cac98b1
JulioS <juliosousa@sapo.pt>
2024-02-22 12:40:49 +0000

    retirar o url da imagem em tamanho real

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-//  function openImage(imageUrl) {
-//   var windowName = 'ManualImageWindow';
-//   var windowFeatures = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=800,height=600';
-//   window.open(imageUrl, '_blank', 'toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=800,height=600');
-//  }
 
-// teste abrir a imagem e ajustar a janela à dimensão da foto e dar nome à janela 
 function openImage(imageUrl) {
-  
   var img = new Image();
   
   img.onload = function() {
     var imageName = imageUrl.split('/').pop(); 
     document.title = imageName;
+    
     var width = img.width;
     var height = img.height;
-    var windowFeatures = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=' + width + ',height=' + height;
-    window.open(imageUrl, '_blank', windowFeatures);
+    
+    var newTab = window.open('', '_blank', 'width=' + width + ', height=' + height + ', top=0, left=0');
+    newTab.document.write('<html><head><title>' + imageName + '</title></head><body style="margin: 0;"><img src="' + imageUrl + '" width="' + width + '" height="' + height + '" style="display: block;"></body></html>');
   };
   
   img.src = imageUrl;
 }
-
-
