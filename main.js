046e9c7c9e2b87bc5c5a8874180e2c32cd2666cb
JulioS <juliosousa@sapo.pt>
2024-02-22 12:07:08 +0000

    dar nome da imagem à janela do browser

--- main.js
+++ main.js
@@ -4,16 +4,20 @@
 //   window.open(imageUrl, '_blank', 'toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=800,height=600');
 //  }
 
-// teste abrir a imagem e ajustar a janela à dimensão da foto
+// teste abrir a imagem e ajustar a janela à dimensão da foto e dar nome à janela 
 function openImage(imageUrl) {
+  
   var img = new Image();
   
   img.onload = function() {
+    var imageName = imageUrl.split('/').pop(); 
+    document.title = imageName;
     var width = img.width;
     var height = img.height;
     var windowFeatures = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=' + width + ',height=' + height;
     window.open(imageUrl, '_blank', windowFeatures);
   };
+  
   img.src = imageUrl;
 }
 
