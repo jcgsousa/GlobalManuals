03ccee1c9c1b5ff173ab12f53dbda72213aa2305
JulioS <juliosousa@sapo.pt>
2024-02-22 17:58:26 +0000

    teste new way zoom

--- main.js
+++ main.js
@@ -15,3 +15,9 @@ function openImage(imageUrl) {
   
   img.src = imageUrl;
 }
+function abrirPaginaTeste(imagem) {
+  var novaJanela = window.open('', '_blank', 'toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no');
+  novaJanela.document.write('<html lang="pt-pt"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Zoom Image</title><style>body{margin:0;padding:0;display:flex;justify-content:center;align-items:center;height:100vh;overflow:hidden;}img{max-width:100%;max-height:100%;}</style></head><body><ul><div><p><img id="thumbnail01" src="' + imagem + '" alt="Thumbnail Image" /></p></div></ul></body></html>');
+  
+  novaJanela.document.close();
+}
